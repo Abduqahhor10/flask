445a0d2950ac568777460bf9139e2ef9cc884fbb
Abdu_qahhor_2o1o <asunnataliyev10@gmail.com>
2025-09-28 21:16:06 +0500

    range

--- models.py
+++ models.py
@@ -9,11 +9,11 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    profile_image = db.Column(db.String(150), nullable=True, default="default.png")  # yangi ustun
+
+    profile_image = db.Column(db.String(150), nullable=True, default="default.png")  # 👈 qo‘shildi
 
     blogs = db.relationship('Blog', backref='author', lazy=True)
     comments = db.relationship('Comment', backref='user', lazy=True)
-
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
